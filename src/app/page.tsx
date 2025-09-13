98564d2489fa667a6a1b895263e0f34107449f22
adexfit <y.adeboye@gmail.com>
2025-09-13 14:32:32 +0100

    updated navbar component and page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,32 @@
-import Image from "next/image";
-import { Button } from "@/components/ui/button";
+import AppAreaChart from "@/components/AppAreaChart";
+import AppBarChart from "@/components/AppBarChart";
+import AppPieChart from "@/components/AppPieChart";
+import CardList from "@/components/CardList";
+import TodoList from "@/components/TodoList";
 
-export default function Home() {
+const Homepage = () => {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <p className="text-amber-700">Hello world</p>
-      <Button size="lg" variant="destructive">
-        Click me
-      </Button>
+    <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-4 gap-4">
+      <div className="bg-primary-foreground p-4 rounded-lg lg:col-span-2 xl:col-span-1 2xl:col-span-2">
+        <AppBarChart />
+      </div>
+      <div className="bg-primary-foreground p-4 rounded-lg">
+        <CardList title="Latest Transactions" />
+      </div>
+      <div className="bg-primary-foreground p-4 rounded-lg">
+        <AppPieChart />
+      </div>
+      <div className="bg-primary-foreground p-4 rounded-lg">
+        <TodoList />
+      </div>
+      <div className="bg-primary-foreground p-4 rounded-lg lg:col-span-2 xl:col-span-1 2xl:col-span-2">
+        <AppAreaChart />
+      </div>
+      <div className="bg-primary-foreground p-4 rounded-lg">
+        <CardList title="Popular Content" />
+      </div>
     </div>
   );
-}
+};
+
+export default Homepage;
